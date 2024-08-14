1104931e3d4558524fc76810aa9310de31ce3c94
daurdos <daurdos@gmail.com>
2024-08-14 05:18:03 +0500

    Stmdout

--- Deg.Edportal/Models/Student.cs
+++ Deg.Edportal/Models/Student.cs
@@ -11,6 +11,7 @@ namespace Deg.Edportal
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public  string FirstNameKaz { get; set; }
 
     }
 }
